9dc693ef52978089d30668781ea77f73e5051c74
unknown <bartoluche.matias@gmail.com>
2025-06-17 15:30:17 -0300

    Agregando el script en un archivo externo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("prueba js");
+window.addEventListener('DOMContentLoaded', () => {
+    const modeloTierra = document.querySelector("#modeloTierra");
+    const modeloLuna = document.querySelector("#modeloLuna");
+    const modeloSol = document.querySelector("#modeloSol");
+
+    document.querySelector("#marcadorTierra").addEventListener("markerFound", () => {
+        console.log("TIERRA detectada");
+        modeloTierra.setAttribute("visible", "true");
+        modeloLuna.setAttribute("visible", "false");
+        modeloSol.setAttribute("visible", "false");
+    });
+
+    document.querySelector("#marcadorLuna").addEventListener("markerFound", () => {
+        console.log("LUNA detectada");
+        modeloLuna.setAttribute("visible", "true");
+        modeloTierra.setAttribute("visible", "false");
+        modeloSol.setAttribute("visible", "false");
+    });
+
+    document.querySelector("#marcadorSol").addEventListener("markerFound", () => {
+        console.log("SOL detectada");
+        modeloLuna.setAttribute("visible", "false");
+        modeloTierra.setAttribute("visible", "false");
+        modeloSol.setAttribute("visible", "true");
+    }); 
+
+});
+
