95f1a1dffdb568b2d3eb5943967e58f13141a6c6
unknown <bartoluche.matias@gmail.com>
2025-06-20 16:31:09 -0300

    Agregando evento 'markerLost' a cada marcador para ocultar el modelo cuando el marcador sale de camara

--- js/script.js
+++ js/script.js
@@ -7,24 +7,19 @@ window.addEventListener('DOMContentLoaded', () => {
     const lunaSistema = document.querySelector("#lunaSistema");
     const solSistema = document.querySelector("#solSistema");
 
+/*-------------------------------- mostrar modelos de planetas ----------------------------------------*/
     document.querySelector("#marcadorTierra").addEventListener("markerFound", () => {
         console.log("TIERRA detectada");
         modeloTierra.setAttribute("visible", "true");
-        modeloLuna.setAttribute("visible", "false");
-        modeloSol.setAttribute("visible", "false");
     });
 
     document.querySelector("#marcadorLuna").addEventListener("markerFound", () => {
         console.log("LUNA detectada");
         modeloLuna.setAttribute("visible", "true");
-        modeloTierra.setAttribute("visible", "false");
-        modeloSol.setAttribute("visible", "false");
     });
 
     document.querySelector("#marcadorSol").addEventListener("markerFound", () => {
         console.log("SOL detectada");
-        modeloLuna.setAttribute("visible", "false");
-        modeloTierra.setAttribute("visible", "false");
         modeloSol.setAttribute("visible", "true");
     });
 
@@ -37,5 +32,34 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector("#marcadorFases").addEventListener("markerFound", () => {
         console.log("Fases de la luna detectada");
-    });    
+    });  
+/*------------------------------------------------------------------------------------------------------*/
+    
+
+/*--------------------------------- ocultar modelos de planetas ----------------------------------------*/
+    document.querySelector('#marcadorTierra').addEventListener('markerLost', () => {
+        console.log("TIERRA ocultada");
+        modeloTierra.setAttribute("visible", "false");
+    });
+
+    document.querySelector('#marcadorLuna').addEventListener('markerLost', () => {
+        console.log("LUNA ocultada");
+        modeloLuna.setAttribute("visible", "false");
+    });
+
+    document.querySelector('#marcadorSol').addEventListener('markerLost', () => {
+        console.log("SOL ocultada");
+        modeloSol.setAttribute("visible", "false");
+    });
+
+    document.querySelector("#marcadorSistema").addEventListener("markerLost", () => {
+        console.log("SISTEMA oculto");
+        tierraSistema.setAttribute("visible", "false");
+        lunaSistema.setAttribute("visible", "false");
+        solSistema.setAttribute("visible", "false");
+    });
+
+    document.querySelector("#marcadorFases").addEventListener("markerLost", () => {
+        console.log("Fases de la luna ocultada");
+    });  
 });
