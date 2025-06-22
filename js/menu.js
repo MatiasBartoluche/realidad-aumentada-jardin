a11eba3020a06bea6d66e1333f437720cc017f4a
unknown <bartoluche.matias@gmail.com>
2025-06-22 17:26:03 -0300

    Aplicando javascript para mostrar y ocultar el menu

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,15 @@
-console.log("hola");
+window.addEventListener('DOMContentLoaded', () => {
+
+    openSidebar();
+    closeSidebar();
+});
+
+const navbar = document.getElementById('navbar');
+
+function openSidebar(){
+    navbar.classList.add('show');
+}
+
+function closeSidebar(){
+    navbar.classList.remove('show');
+}
